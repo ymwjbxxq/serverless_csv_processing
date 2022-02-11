751ab73c1d5cf04a1dd5e0a624252315cf301e3b
Daniele <>
2022-02-11 15:08:41 +0100

    use StreamReader instead of reading all into memory

--- src/bin/handler.rs
+++ src/bin/handler.rs
@@ -1,10 +1,11 @@
-use csv;
 use lambda_runtime::{handler_fn, Context, Error};
 use rust_csv_processig::{
     aws::client::{AWSClient, AWSConfig},
     dtos::record::Record,
     dtos::s3_event::S3Event,
 };
+use tokio_stream::StreamExt;
+use tokio_util::io::StreamReader;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -24,27 +25,7 @@ async fn main() -> Result<(), Error> {
 pub async fn execute(aws_client: &AWSClient, event: S3Event, _ctx: Context) -> Result<(), Error> {
     println!("{:?}", event);
 
-    let data = download_file(aws_client, &event).await?;
-    let buffer = &data[..];
-
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .delimiter(b'\t')
-        .double_quote(false)
-        .escape(Some(b'\\'))
-        .flexible(true)
-        .from_reader(buffer);
-
-    for result in rdr.deserialize() {
-        let record: Record = result?;
-        // do something with record
-    }
-
-    Ok(())
-}
-
-async fn download_file(aws_client: &AWSClient, event: &S3Event) -> Result<bytes::Bytes, Error> {
-    let result = aws_client
+    let stream = aws_client
         .s3_client
         .as_ref()
         .unwrap()
@@ -52,8 +33,31 @@ async fn download_file(aws_client: &AWSClient, event: &S3Event) -> Result<bytes:
         .bucket(event.bucket.name.to_string())
         .key(event.object.key.to_string())
         .send()
-        .await?;
+        .await?
+        .body
+        .map(|result| result.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)));
 
-    let data = result.body.collect().await?.into_bytes();
-    Ok(data)
+    // Convert the stream into an AsyncRead
+    let stream_reader = StreamReader::new(stream);
+
+    // Create a CSV reader
+    let mut csv_reader = csv_async::AsyncReaderBuilder::new()
+        .has_headers(true)
+        .delimiter(b'\t')
+        .double_quote(false)
+        .escape(Some(b'\\'))
+        .flexible(true)
+        .create_deserializer(stream_reader);
+
+    let mut count = 0;
+    // Iterate over the CSV rows
+    let mut records = csv_reader.deserialize::<Record>();
+    while let Some(record) = records.next().await {
+        let _record: Record = record?;
+        count += 1;
+    }
+
+    println!("COMPLETED {:?}", count);
+
+    Ok(())
 }
